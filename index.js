5c9f5b4fccac756125bb349528327776d9efa638
Rishikesh Kumar <rishi7258prince@gmail.com>
2021-01-12 18:54:42 +0530

    Added color picker which can change the color

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("you are doing great!");
+
+function setcolor()
+{
+    var picker = document.getElementById("color");
+    var log = document.getElementById('log');
+    var blocks = document.getElementsByClassName("block");
+    for (let i = 0; i < 9; i++)
+    {
+        console.log(blocks[i]);
+        blocks[i].style.backgroundColor = picker.value;
+    }
+    log.innerHTML +=`<p>The color of the board is changed to ${picker.value}</p>`;
+    console.log(picker.value);
+}

ba23b202189bd9de83cee1ad6c8332a23704e156
Rishikesh Kumar <rishi7258prince@gmail.com>
2021-01-14 15:46:41 +0530

    Added some buttons and now the blocks can be selected and filled

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 console.log("you are doing great!");
-var boardcolor, bordercolor;
+let boardcolor = "#b8860b";
+let bordercolor = "#ff0000";
+let fillmode;
+
+function reset()
+{
+    location.reload();
+}
 
 function setcolor()
 {
@@ -39,4 +46,40 @@ function setborder()
     bordercolor = picker.value;
     log.innerHTML +=`<p>Color of the border is changed to ${picker.value}</p>`;
     // console.log(picker.value);
+}
+
+function select(e)
+{
+    let blocks = document.getElementsByClassName("block")
+    let log = document.getElementById("log");
+    for (let i = 0; i < 9; i++)
+    {
+        blocks[i].style.backgroundColor = boardcolor;
+        blocks[i].style.borderColor = bordercolor
+    }
+    e.style.backgroundColor = "green";
+    e.style.borderColor = "darkgreen";
+    e.innerHTML = "X";
+    log.innerHTML += `<p>${e.className} is filled with ${e.innerHTML}`
+}
+
+function flipcoin()
+{
+    let num = Math.random()*100;
+    console.log(num);
+    
+    let winner;
+    let log = document.getElementById('log');
+    if (num > 50)
+    {
+        winner = "player1";
+    }
+    else
+    {
+        winner = "player2";
+    }
+
+    log.innerHTML += `<p>${winner} won the toss</p>`;
+    fillmode = prompt("you have won the toss choose the input method ('0' of 'X'))");
+    console.log(fillmode);
 }

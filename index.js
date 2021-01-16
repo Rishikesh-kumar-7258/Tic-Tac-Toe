6e54003d1ca4d5f534eb05f60ee5d61529077afb
Rishikesh Kumar <rishi7258prince@gmail.com>
2021-01-16 10:32:30 +0530

    Added player function and a little more responsive

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ console.log("you are doing great!");
 let boardcolor = "#b8860b";
 let bordercolor = "#8b0000";
 let fillmode;
+let player1 = true, player2 = false;
 
 //defining reset function which reloads the page
 function reset()
 {
     location.reload();
+    
 }
 
 //defining setcolor function which set's the color to the board
@@ -65,28 +67,89 @@ function select(e)
     }
     e.style.backgroundColor = "green";
     e.style.borderColor = "darkgreen";
-    e.innerHTML = "X";
-    log.innerHTML += `<p>${e.className} is filled with ${e.innerHTML}`
-}
-
-//defining a function for a virtual toss 
-function flipcoin()
-{
-    let num = Math.random()*100;
-    console.log(num);
-    
-    let winner;
-    let log = document.getElementById('log');
-    if (num > 50)
+    if (e.innerHTML == 'X' || e.innerHTML == '0')
     {
-        winner = "player1";
+        return false;
+    }
+    if (player1)
+    {
+        e.innerHTML = "0";
+        player1 = false;
+        player2 = true;
     }
     else
     {
-        winner = "player2";
+        e.innerHTML = "X";
+        player1 = true;
+        player2 = false;
+    }
+    log.innerHTML += `<p>${e.className} is filled with ${e.innerHTML}`
+    inact();
+    // if (horizontal()) console.log("game over");
+    gameover();
+}
+
+//defining a function to set a button inactive
+function inact()
+{
+    let btn1 = document.getElementById('player1');
+    let btn2 = document.getElementById('player2');
+
+    if (player1)
+    {
+        btn2.style.backgroundColor = "grey";
+        btn1.style.backgroundColor = "#ffffff";
+    }
+    else 
+    {
+        btn1.style.backgroundColor = "grey";
+        btn2.style.backgroundColor = "#ffffff";
     }
 
-    fillmode = prompt("you have won the toss choose the input method ('0' or 'X'))");
-    log.innerHTML += `<p style="color:orangered;">${winner} won the toss and has chosen ${fillmode} to fill in the blocks.</p>`;
-    console.log(fillmode);
+}
+
+//a function to determine if the block is empty or not
+function isempty(x)
+{
+    if (x != 'X' && x != '0') return true;
+
+    return false;
+}
+
+//function to determine if the array is equal
+function arEqual(arr)
+{
+    if (arr[0] == arr[1] && arr[1] == arr[2]) return true;
+
+    return false;
+}
+
+// horizontal function which checks the horizontal winner;
+function horizontal()
+{
+    let blocks = document.getElementsByClassName("block");
+
+    for (let i = 0; i < 3; i++)
+    {
+        let arr = [];
+        for (let j = 3*i; j < 3*(i+1); j++)
+        {
+            let filled = blocks[j].innerHTML;
+
+            if (!isempty(filled)) arr.push(filled);
+            else return false;
+        }
+
+        return arEqual(arr);
+    }
+
+}
+
+// function to finish the game
+function gameover()
+{
+    if(horizontal() == true) 
+    {
+        reset();
+    }
 }

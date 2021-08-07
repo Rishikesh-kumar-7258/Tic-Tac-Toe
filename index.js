30b3b9d9488f1b82aaacb3eda2428b0d135802be
Rishikesh-kumar-7258 <69416888+Rishikesh-kumar-7258@users.noreply.github.com>
2021-08-07 16:48:39 +0530

    changed the game logic to class based approach

--- index.js
+++ index.js
@@ -1,3 +1,75 @@
+// class bases approach to solve the problem
+class Matrix {
+    #board;
+    #spaces;
+    #over;
+    constructor() {
+        this.#board = [];
+        this.#spaces = 9;
+        this.#over = -1;
+
+        for (let i = 0; i < 3; i++) this.#board.push(['', '', '']);
+    }
+
+    isOver = () => this.#over;
+
+    insert_to_board = (row, col, value) => {
+        this.#board[row][col] = value;
+        this.#spaces -= 1;
+
+        this.#over = this.#game_over(this.#board,row, col);
+    }
+
+    //function to determine if the array is equal
+    #areEqual(arr) {
+
+        return arr.every(v => v === arr[0]);
+    }
+
+    #game_over(m, row, col) {
+        let horizontal = this.#areEqual(m[row]);
+        if (horizontal) return 1;
+
+        let arr = [];
+        for (let i = 0; i < 3; i++) arr.push(m[i][col]);
+        let vertical = this.#areEqual(arr);
+        if (vertical) return 1;
+
+        let diagonal;
+        if (row === col) {
+            arr = [];
+            for (let i = 0; i < 3; i++) arr.push(m[i][i]);
+
+            diagonal = this.#areEqual(arr);
+
+            if (diagonal) return 1;
+        }
+        if ((row + col) === 2) {
+            arr = [];
+            for (let i = 0; i < 3; i++) arr.push(m[i][2 - i]);
+
+            diagonal = this.#areEqual(arr);
+
+            if (diagonal) return 1;
+        }
+
+        if (this.#spaces == 0) return 0;
+
+        return -1;
+    }
+
+    // Adding functinality for players to play versus computers
+    // computer_choice = (m, sp) => {
+
+    //     if (sp == 0) return 0;
+        
+    // }
+
+}
+
+// Instantiating a board for game
+const board = new Matrix();
+
 //making some variables for the code
 let boardcolor = "#b8860b";
 let bordercolor = "#8b0000";
@@ -20,7 +92,7 @@ function setcolor() {
     let l = document.createElement('p');
 
     if (bordercolor == picker.value) {
-        l.innerText += `same color cannot be given to both board and border`;
+        l.innerText = `same color cannot be given to both board and border`;
         log.append(l);
         return false;
     }
@@ -30,7 +102,7 @@ function setcolor() {
     }
     boardcolor = picker.value;
 
-    l.innerHTML = `Color of the blocks is changed to ${picker.value}`;
+    l.innerText = `Color of the blocks is changed to ${picker.value}`;
     log.append(l);
 }
 
@@ -58,13 +130,6 @@ let h4 = document.querySelector('#fill_opt h4');
 let before = null;
 
 // Using data structures to solve the tic-tac-toe problem
-const matrix = [];
-for (let i = 0; i < 3; i++) {
-    const temp = [];
-    for (let j = 0; j < 3; j++) temp.push('');
-    matrix.push(temp);
-}
-
 blocks.forEach(element => {
     element.addEventListener('click', () => {
         element.classList.add("selected");
@@ -77,67 +142,50 @@ blocks.forEach(element => {
         let add = document.createElement('p');
         if (player1) {
             add.innerText = '0';
+            h4.innerText = "Player2(X)'s turn";
         }
         else {
             add.innerText = 'X';
+            h4.innerText = "Player1(0)'s turn";
         }
 
         player1 = !player1;
 
-        if (player1) h4.innerText = "Player1(0)'s turn";
-        else h4.innerText = "Player2(X)'s turn";
-
         element.append(add);
-        insert_to_matrix(element);
+        board.insert_to_board(get_row(element), get_col(element), element.innerText);
 
         let l = document.createElement('p');
         l.innerText = `${element.className} is filled with ${element.innerText}`;
         log.append(l);
-        gameover(element);
+        gameover();
 
     });
 });
 
-//function to determine if the block is empty or not
-function isempty(x) {
-
-    return (x == '');
-}
-
-//function to determine if the array is equal
-function arEqual(...arg) {
-
-    for (let i = 1; i < arg.length ; i++)
-        if (arg[i] !== arg[i-1]) return false;
-
-    return true;
-}
 
 // function to finish the game
-function gameover(e) {
-    let row = get_row(e);
-    let col = get_col(e);
+function gameover() {
 
-    let horizontal = arEqual(matrix[row][0], matrix[row][1], matrix[row][2]);
-    let vertical = arEqual(matrix[0][col], matrix[1][col], matrix[2][col]);
-
-    let diagonal, temp1 = false, temp2 = false;
-    if (row === col) temp1 =  arEqual(matrix[0][0], matrix[1][1], matrix[2][2]);
-    if ((row + col) === 2) temp2 = arEqual(matrix[0][2], matrix[1][1], matrix[2][0]);
-
-    diagonal = (temp1 || temp2);
-
-    if (horizontal || vertical || diagonal) {
+    if (board.isOver() == 1) {
         let winner;
         if (player1) winner = "player2";
         else winner = "player1";
         setTimeout(() => {
-            alert(`Congratulations! ${winner} has won the game now the game will be restarted`);
+            alert(`Congratulations! ${winner} has won the game\nRestart`);
             reset();
         }, 100);
     }
+
+    if (board.isOver() == 0) {
+        setTimeout(() => {
+            alert(`The game ended in a tie\nRestart`);
+            reset();
+        }, 100);
+    }
+
 }
 
+// functions to get the row and column of the board
 const alphaToInt = e => {
 
     return (e.charCodeAt(0) - 'a'.charCodeAt(0));
@@ -147,7 +195,6 @@ const digitToInt = e => {
 
     return (e.charCodeAt(0) - '0'.charCodeAt(0));
 }
-
 const get_row = box => {
     let classes = box.classList;
     let row = digitToInt(classes[2]) - 1;
@@ -161,12 +208,3 @@ const get_col = box => {
 
     return col;
 }
-
-const insert_to_matrix = box => {
-
-    let row = get_row(box);
-    let col = get_col(box);
-
-    matrix[row][col] = box.innerText;
-}
-

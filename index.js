242b166ebc26407c3efb45518e365cbbddf7fcd9
Rishikesh-kumar-7258 <69416888+Rishikesh-kumar-7258@users.noreply.github.com>
2021-08-07 11:35:45 +0530

    Used data structures in js and mixins in scss

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// console.log("you are doing great!");
-
 //making some variables for the code
 let boardcolor = "#b8860b";
 let bordercolor = "#8b0000";
@@ -13,7 +11,7 @@ function reset() {
 
 // some useful elements to be used in the code
 let log = document.getElementById("log");
-let blocks = document.querySelectorAll(".block")
+let blocks = document.querySelectorAll(".block");
 
 //defining setcolor function which set's the color to the board
 function setcolor() {
@@ -59,86 +57,77 @@ function setborder() {
 let h4 = document.querySelector('#fill_opt h4');
 let before = null;
 
+// Using data structures to solve the tic-tac-toe problem
+const matrix = [];
+for (let i = 0; i < 3; i++) {
+    const temp = [];
+    for (let j = 0; j < 3; j++) temp.push('');
+    matrix.push(temp);
+}
+
 blocks.forEach(element => {
     element.addEventListener('click', () => {
         element.classList.add("selected");
         if (before) before.classList.remove('selected');
         before = element;
 
-        if (!isempty(element)) {
-            return false;
-        }
+
+        if (element.hasChildNodes()) return;
+
         let add = document.createElement('p');
         if (player1) {
             add.innerText = '0';
-            player1 = false;
         }
         else {
             add.innerText = 'X';
-            player1 = true;
         }
+
+        player1 = !player1;
 
         if (player1) h4.innerText = "Player1(0)'s turn";
         else h4.innerText = "Player2(X)'s turn";
 
         element.append(add);
+        insert_to_matrix(element);
 
         let l = document.createElement('p');
         l.innerText = `${element.className} is filled with ${element.innerText}`;
         log.append(l);
-        gameover();
+        gameover(element);
+
     });
 });
 
 //function to determine if the block is empty or not
 function isempty(x) {
-    if (x.hasChildNodes()) return false;
-    else return true;
+
+    return (x == '');
 }
 
 //function to determine if the array is equal
 function arEqual(...arg) {
 
-    let empty = false;
-    for (let i = 0; i < arg.length; i++)
-    {
-        if (isempty(arg[i])) return false;
-    }
-
-    for (let i = 1; i < arg.length; i++) {
-        if (arg[i].innerText !== arg[i-1].innerText) return false;
-    }
+    for (let i = 1; i < arg.length ; i++)
+        if (arg[i] !== arg[i-1]) return false;
 
     return true;
 }
 
-// horizontal function which checks the horizontal winner;
-function horizontal() {
-    
-    return (arEqual(blocks[0], blocks[1], blocks[2]) 
-            || arEqual(blocks[3], blocks[4], blocks[5])
-            || arEqual(blocks[6], blocks[7], blocks[8]));
-
-}
-
-//function to find check the winner vertically
-function vertically() {
-    
-    return (arEqual(blocks[0], blocks[3], blocks[6])
-        || arEqual(blocks[1], blocks[4], blocks[7])
-        || arEqual(blocks[2], blocks[5], blocks[8]));
-}
-
-//function to check the winner diagonally
-function diagonally() {
-    
-    return (arEqual(blocks[0], blocks[4], blocks[8])
-        || arEqual(blocks[2], blocks[4], blocks[6]));
-}
-
 // function to finish the game
-function gameover() {
-    if (horizontal() || vertically() || diagonally()) {
+function gameover(e) {
+    let row = get_row(e);
+    let col = get_col(e);
+
+    let horizontal = arEqual(matrix[row][0], matrix[row][1], matrix[row][2]);
+    let vertical = arEqual(matrix[0][col], matrix[1][col], matrix[2][col]);
+
+    let diagonal, temp1 = false, temp2 = false;
+    if (row === col) temp1 =  arEqual(matrix[0][0], matrix[1][1], matrix[2][2]);
+    if ((row + col) === 2) temp2 = arEqual(matrix[0][2], matrix[1][1], matrix[2][0]);
+
+    diagonal = (temp1 || temp2);
+
+    if (horizontal || vertical || diagonal) {
         let winner;
         if (player1) winner = "player2";
         else winner = "player1";
@@ -148,3 +137,36 @@ function gameover() {
         }, 100);
     }
 }
+
+const alphaToInt = e => {
+
+    return (e.charCodeAt(0) - 'a'.charCodeAt(0));
+}
+
+const digitToInt = e => {
+
+    return (e.charCodeAt(0) - '0'.charCodeAt(0));
+}
+
+const get_row = box => {
+    let classes = box.classList;
+    let row = digitToInt(classes[2]) - 1;
+
+    return row;
+}
+
+const get_col = box => {
+    let classes = box.classList;
+    let col = alphaToInt(classes[1]);
+
+    return col;
+}
+
+const insert_to_matrix = box => {
+
+    let row = get_row(box);
+    let col = get_col(box);
+
+    matrix[row][col] = box.innerText;
+}
+
